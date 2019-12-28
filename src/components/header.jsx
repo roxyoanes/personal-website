8dffa468d3924589bb2fc3ff91367b84e5c049ea
roxioanes <roxioanes11@gmail.com>
2019-12-28 12:14:48 +0100

    Add styled components

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,33 +2,19 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import StyledHeader from "./styled-components/styled-header";
+
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+    <StyledHeader>
+      <h1 className="title">
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
         >
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </header>
+    </StyledHeader>
 )
 
 Header.propTypes = {
---
+++ src/components/styled-components/styled-header.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const StyledHeader = styled.div`
+  color: red;
+
+  .title {
+    color: pink;
+  }
+`;
+
+export default StyledHeader;
