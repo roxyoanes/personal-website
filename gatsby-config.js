cb9a0dc2a1d159fcae97831a21a9edcf22555283
roxioanes <roxioanes11@gmail.com>
2020-01-16 20:44:47 +0100

    Add description website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Roxana`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `RoxanaOanes`,
+    description: `Personal website of Roxana Oanes, web developer.`,
+    author: `@OanesRoxana`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
